f4a49ab059219ea15a909f39e05e1273a8b88901
kex121 <t1mursobolev@yandex.ru>
2024-10-11 16:58:39 +0700

    db function done

---
+++ crud/DB-function.js
@@ -0,0 +1,46 @@
+const { Enemies, User, resultGame } = require('../db/models');
+
+async function emojiEnemies() {
+  try {
+    const randomId = Math.floor(Math.random() * 12) + 1;
+    const enemy = await Enemies.findOne({ where: { id: randomId } });
+    return enemy.emoji;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const name = process.argv[2];
+
+async function checkNameUser(name) {
+  try {
+    const checkUser = await User.findOne({ where: { name } });
+    if (checkUser) {
+      resultGame(checkUser.id)
+    } else {
+      const newUser = await User.create({ name });
+      resultGame(newUser.id)
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function resultGame(user_id, gameTime = 0, killedEnemy = 0) {
+    try {
+        await resultGame.create({ gameTime, killedEnemy, user_id });
+        console.log(`Игра окончена!\nСчет:${gameTime}\nКоличество убитых врагов:${killedEnemy}\nВаш результат сохранен под номером ${resultGame.id}`);
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+async function getResultGameID(name) {
+    try {
+    if(typeof Number(name) === 'number') {
+        const p = await resultGame.findOne({ where: { id: name } });
+    }
+    } catch(err) {
+        console.error(err)
+    }
+}
