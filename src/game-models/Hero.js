bd4098cde86112c7e8943c630bb3a9bc2c4d1b79
PolinaPavlova <polinapavlova174@gmail.com>
2024-10-11 14:25:32 +0500

    regenerate track

--- src/game-models/Hero.js
+++ src/game-models/Hero.js
@@ -1,29 +1,32 @@
-// Наш герой.
+const Boomerang = require("./Boomerang");
 
 class Hero {
   constructor({ position }) {
-    this.skin = '🤠'; // можете использовать любые emoji '💃'
+    this.skin = "🤠"; // можете использовать любые emoji '💃'
     this.position = position;
+    this.boomerang = null;
   }
 
   moveLeft() {
-    // Идём влево.
-    this.position -= 1;
+    if (this.position !== 0) {
+      this.position -= 1;
+    }
   }
 
-  moveRight() {
-    // Идём вправо.
-    this.position += 1;
+  moveRight(track) {
+    if (this.position < track.length) {
+      this.position += 1;
+    }
   }
 
   attack() {
-    // Атакуем.
+    this.boomerang = new Boomerang(this.position);
     this.boomerang.fly();
   }
 
   die() {
-    this.skin = '💀';
-    console.log('YOU ARE DEAD!💀');
+    this.skin = "💀";
+    console.log("YOU ARE DEAD!💀");
     process.exit();
   }
 }
